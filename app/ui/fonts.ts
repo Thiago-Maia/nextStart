c8925c451b00a11cc989a6b910dd1a25567bb740
Thiago Maia <tgmaia.fc@gmail.com>
2024-05-04 17:35:35 -0300

    primeiro commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana, Comic_Neue } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({ weight: ["400","700"], subsets:["latin"] });
+
+export const comic_Neue = Comic_Neue({ weight: ["400","700"], subsets:["latin"] });
